dd971c444ddf0c348c7996d69a30c6faf2c11c15
j-ornelas <johnnyoh1988@gmail.com>
2018-03-28 12:53:43 -0700

    Adds HTML templates

---
+++ templates/app.js
@@ -0,0 +1,6 @@
+module.exports = (description, reviews, bookings, neighborhood) => `
+  <div id="description-service">${description}<div>
+  <div id="reviews-service">${reviews}<div>
+  <div id="bookings-service">${bookings}<div>
+  <div id="neighborhood">${neighborhood}<div>
+`;
