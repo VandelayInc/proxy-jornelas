31cc28c127f8e1ffe81e43a731b00f61f897b0f1
j-ornelas <johnnyoh1988@gmail.com>
2018-03-29 17:15:00 -0700

    Makes deployed proxy look like local proxy

--- templates/app.js
+++ templates/app.js
@@ -1,6 +1,24 @@
 module.exports = (description, reviews, bookings, neighborhood) => `
-  <div id="description-service">${description}<div>
-  <div id="reviews-service">${reviews}<div>
-  <div id="bookings-service">${bookings}<div>
-  <div id="neighborhood">${neighborhood}<div>
+<div class='full-listing-page'>
+  <div class='top-bar'><img src='../../services/8.png'><img src='../../services/9.png'></div>
+  <div class='pictures' id='photo-carousel-service'><img src='../../services/listing_photo.jpg'></div>
+  <div class='center-column'>
+
+    <div class='details'>
+      <div id='description-service'>${description}
+        <img src='../../services/1.png'>
+        <img src='../../services/2.png'>
+      </div>
+      <div id='reviews-service'><img src='../../services/3.png'></div>
+      <div class='host'><img src='../../services/4.png'></div>
+      <div id='neighborhood'><img src='../../services/5.png'></div>
+    </div>
+
+    <div class='booking' id='bookings-service'><img src='../../services/booking.png'></div>
+
+  </div>
+
+  <div id='listings'><img src='../../services/6.png'></div>
+  <div class='bottom-bar'><img src='../../services/7.png'></div>
+</div>
 `;
